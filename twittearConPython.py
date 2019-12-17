b4d78f0a2b1a952c37dc8be0363b8e9dca4e49db
Agustin Conti <29712330+aagustinconti@users.noreply.github.com>
2019-12-16 23:30:02 -0300

    Add files via upload

--- twittearConPython.py
+++ twittearConPython.py
@@ -5,12 +5,18 @@ import os
 
 
 
+
  
 
 def twitt(texto):
     # Configuracion de acceso con las credenciales
-    auth = tweepy.OAuthHandler(os.environ.get('CONSUMER_KEY'), os.environ.get('ACCESS_TOKEN'))
-    auth.set_access_token(os.environ.get('ACCESS_TOKEN'), os.environ.get('ACCESS_TOKEN_SECRET') )
+    CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
+    CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
+    ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
+    ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET')
+    
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
   
     # Listos para hacer la conexion con el API
     api = tweepy.API(auth)
